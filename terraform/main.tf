a0908261df012e111f4d98b6c9bac939fc0787af
shohrabuddin <shobuzch@gmail.com>
2022-10-02 01:20:59 +0200

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,17 +27,17 @@ terraform {
 
 # Locate the existing resource group
 # The resource group name should be the same as the storage account that is used in terraform backend block above
-/*data "azurerm_resource_group" "main" {
-  name = "${var.resource_group}"
-}*/
+data "azurerm_resource_group" "main" {
+  name = "myInitialRG"
+}
 
 # If new resource group is created and used in the subsequent resources (VM, appservice, NSG etc.) terraform will first try to delete the resource group assiciated with
 # storage account that is used in erraform backend block above. 
-module "resource_group" {
+/*module "resource_group" {
   source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
-}
+}*/
 
 /*resource "azurerm_resource_group" "main" {
   name     = "${var.resource_group}"
@@ -51,7 +51,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "myNetwork2"
-  resource_group       = module.resource_group.resource_group_name //resource_group_name is the output varible name in resource_group/output.tf file
+  resource_group       = data.azurerm_resource_group.main.name //module.resource_group.resource_group_name resource_group_name is the output varible name in resource_group/output.tf file
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -60,7 +60,7 @@ module "nsg-test" {
   location              = "${var.location}"
   application_type      = "${var.application_type}"
   resource_type         = "NSG"
-  resource_group        = module.resource_group.resource_group_name //resource_group_name is the output varible name in resource_group/output.tf file
+  resource_group        = data.azurerm_resource_group.main.name //resource_group_name is the output varible name in resource_group/output.tf file
   subnet_id             = "${module.network.subnet_id_test}"
   address_prefix_test   = "${var.address_prefix_test}"
 }
@@ -69,20 +69,20 @@ module "appservice" {
   location               = "${var.location}"
   application_type       = "${var.application_type}"
   resource_type          = "AppService2"
-  resource_group         = module.resource_group.resource_group_name //resource_group_name is the output varible name in resource_group/output.tf file
+  resource_group         = data.azurerm_resource_group.main.name //resource_group_name is the output varible name in resource_group/output.tf file
 }
 module "publicip" {
   source                 = "./modules/publicip"
   location               = "${var.location}"
   application_type       = "${var.application_type}"
   resource_type          = "publicip"
-  resource_group         = module.resource_group.resource_group_name //resource_group_name is the output varible name in resource_group/output.tf file
+  resource_group         = data.azurerm_resource_group.main.name //resource_group_name is the output varible name in resource_group/output.tf file
 }
 
 module "virtual-machine" {
   source                 = "./modules/vm"
   application_type       = "${var.application_type}"
-  resource_group         = module.resource_group.resource_group_name //resource_group_name is the output varible name in resource_group/output.tf file
+  resource_group         = data.azurerm_resource_group.main.name //resource_group_name is the output varible name in resource_group/output.tf file
   resource_type          = "myVM3"
   location               = "${var.location}"
   subnet_id              = "${module.network.subnet_id_test}"
